8da3fa98f3582178683ac4688520f611c192f6a7
Samuel Matthew <samuelmatthew211@gmail.com>
2021-11-26 12:00:38 +0700

    :lipstick: Create NavBar routing

---
+++ src/components/NavBar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function NavBar() {
+  return (
+    <nav className="navbar">
+      <div className="wrapper py-3 px-5 d-flex flex-row justify-content-between align-items-center">
+        <h1 className="mb-0">Logo</h1>
+        <div className="d-flex flex-row align-items-center">
+          <li className="ms-3">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="ms-3">
+            <Link to="/about">About</Link>
+          </li>
+        </div>
+      </div>
+    </nav>
+  );
+}
---
+++ src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+export default function HomePage() {
+  return (
+    <section
+      id="HomePage"
+      className="page
+    "
+    >
+      <div className="wrapper py-3 px-5 bg-secondary">
+        <p>lorem</p>
+      </div>
+    </section>
+  );
+}
